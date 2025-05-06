899612c050720df93516fcc498cb9e2b0a43479f
dfsierra <danifscuentas@gmail.com>
2025-05-06 11:23:25 -0500

    bp y conexion base de datos

---
+++ app.js
@@ -0,0 +1,20 @@
+// app.js
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const db = require('./config/db'); // Esto importa y ejecuta la conexión
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API de biblioteca funcionando 🎉");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en puerto ${PORT}`);
+});
